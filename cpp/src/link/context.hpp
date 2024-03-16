7a71594397dabb8009931a719cfa4c8077646a9f
Emmanuel Buckshi <adventurist@gmail.com>
2024-03-15 20:28:32 -0400

    run doesnt need to take an argument using poll to set the pace 100ms

--- cpp/src/link/context.hpp
+++ cpp/src/link/context.hpp
@@ -107,7 +107,7 @@ class context
 public:
   static context&     instance();
 
-  bool run(int id);
+  bool run();
   int  get_channel_socket() const;
   void set_channel_socket(int socket_fd);
   bool init(const std::string& token);

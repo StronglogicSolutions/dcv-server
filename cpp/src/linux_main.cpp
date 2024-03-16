9d7efa62535b18f123f62a861f5d6fd0d21e1c52
Emmanuel Buckshi <adventurist@gmail.com>
2024-03-15 20:29:50 -0400

    setting logger to not write to stdout removing sleep (we use poll to block for 100ms) simplifying write function logic

--- cpp/src/linux_main.cpp
+++ cpp/src/linux_main.cpp
@@ -101,16 +101,13 @@ void write_msg(ext_msg_t &msg)
   uint8_t *buf = new uint8_t[msg_sz];
   msg.SerializeToArray(buf, msg_sz);
 
-  if (!write_to_channel(STDOUT_FILENO, reinterpret_cast<uint8_t *>(&msg_sz), sizeof(msg_sz)))
+  if (write_to_channel(STDOUT_FILENO, reinterpret_cast<uint8_t *>(&msg_sz), sizeof(msg_sz)))
   {
-    free(buf);
-    return;
+    write_to_channel(STDOUT_FILENO, buf, msg_sz);
+    fsync(STDOUT_FILENO);
   }
 
-  write_to_channel(STDOUT_FILENO, buf, msg_sz);
-  fsync(STDOUT_FILENO);
-
-  free(buf);
+  delete[] buf;
 }
 
 void DriverOpen()
@@ -202,19 +199,15 @@ void DriverOpen()
     throw std::runtime_error("Failed to open virtual channel");
   }
 
-  // TODO: Print event information
-  // DO something?
-
   delete msg;
-
 
 }
 //-----------------------------------------------------------------
 void DriverRun()
 {
   klog().d("Running driver");
-  while (!ctx().run(0))
-    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+  while (ctx().run())
+    ;
 
 }
 //-----------------------------------------------------------------
@@ -256,7 +249,7 @@ int main()
 {
   try
   {
-    klogger::init("dcv", "trace");
+    klogger::init("dcv", "trace", false);
 
     klog().i("open_virtual_channel");
 
